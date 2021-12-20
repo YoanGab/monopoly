5a6b098c7be0a0c46a31da09dd54d0cfd12d2ec3
Bastien-LDC <bastien.saclay@gmail.com>
2021-12-20 16:42:21 +0100

    C# Solution

---
+++ MONOPOLY_LEDUC_GABISON/MONOPOLY_LEDUC_GABISON/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONOPOLY_LEDUC_GABISON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

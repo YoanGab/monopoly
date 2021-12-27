32f361fec7650a2af53265c433361e4d522228e2
Bastien-LDC <bastien.saclay@gmail.com>
2021-12-27 18:28:22 +0100

    Observers improved & Main simplified

--- MONOPOLY_LEDUC_GABISON/MONOPOLY_LEDUC_GABISON/Program.cs
+++ MONOPOLY_LEDUC_GABISON/MONOPOLY_LEDUC_GABISON/Program.cs
@@ -12,17 +12,7 @@ namespace MONOPOLY_LEDUC_GABISON
         static void Main(string[] args)
         {
             Game monopoly = new Game();
-            monopoly.AddPlayer(new Player("Yoan"));
-            monopoly.AddPlayer(new Player("Bastien"));
-
-            do
-            {
-                monopoly.Play();
-            } while(!monopoly.IsGameOver());
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Congrats {monopoly.GetWinner().Name} ! You're the winner !");
-            Console.ForegroundColor = ConsoleColor.White;
+            monopoly.Play();            
             Console.ReadKey();
         }
     }
--- MONOPOLY_LEDUC_GABISON/MONOPOLY_LEDUC_GABISON/models/Game.cs
+++ MONOPOLY_LEDUC_GABISON/MONOPOLY_LEDUC_GABISON/models/Game.cs
@@ -6,20 +6,25 @@ namespace MONOPOLY_LEDUC_GABISON.models
     public class Game
     {
         // Properties
+        private static Game instance;
         private List<Player> players;
         private int indexCurrentPlayer;
         private Player currentPlayer;
         private int[] dice;
-        private List<Observer> lapObservers, jailObservers;
+
+        public static Game getInstance()
+        {
+            if (instance == null)
+                instance = new Game();
+            return instance;
+        }
 
         // Constructor
         public Game()
         {
             players = new List<Player>();
             currentPlayer = null;
-            dice = new int[2];
-            lapObservers = new List<Observer>();
-            jailObservers = new List<Observer>();
+            dice = new int[2];            
         }
 
         // Getters & Setters
@@ -32,24 +37,6 @@ namespace MONOPOLY_LEDUC_GABISON.models
         public int[] Dice { get => dice; set => dice = value; }
 
         // Methods       
-        void registerLapObservers(Observer o) {
-            lapObservers.Add(o);
-        }
-
-        void registerJailObservers(Observer o) {
-            jailObservers.Add(o);
-        }
-
-        public void notifyLapObservers() {
-            foreach (Observer o in lapObservers)
-                o.update();
-        }
-
-        public void notifyJailObservers() {
-            foreach (Observer o in jailObservers)
-                o.update();
-        }
-
         public void AddPlayer(Player player)
         {
             players.Add(player);
@@ -69,9 +56,28 @@ namespace MONOPOLY_LEDUC_GABISON.models
 
         public void Play()
         {
-            if (players.Count <= 1)
+            string space = "                                  ";
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine(space + "$$\\      $$\\  $$$$$$\\  $$\\   $$\\  $$$$$$\\  $$$$$$$\\   $$$$$$\\  $$\\   $$\\     $$\\       ");
+            Console.WriteLine(space + "$$$\\    $$$ |$$  __$$\\ $$$\\  $$ |$$  __$$\\ $$  __$$\\ $$  __$$\\ $$ |  \\$$\\   $$  |      ");
+            Console.WriteLine(space + "$$$$\\  $$$$ |$$ /  $$ |$$$$\\ $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ |   \\$$\\ $$  /       ");
+            Console.WriteLine(space + "$$\\$$\\$$ $$ |$$ |  $$ |$$ $$\\$$ |$$ |  $$ |$$$$$$$  |$$ |  $$ |$$ |    \\$$$$  /        ");
+            Console.WriteLine(space + "$$ \\$$$  $$ |$$ |  $$ |$$ \\$$$$ |$$ |  $$ |$$  ____/ $$ |  $$ |$$ |     \\$$  /         ");
+            Console.WriteLine(space + "$$ |\\$  /$$ |$$ |  $$ |$$ |\\$$$ |$$ |  $$ |$$ |      $$ |  $$ |$$ |      $$ |          ");
+            Console.WriteLine(space + "$$ | \\_/ $$ | $$$$$$  |$$ | \\$$ | $$$$$$  |$$ |       $$$$$$  |$$$$$$$$\\ $$ |          ");
+            Console.WriteLine(space + "\\__|     \\__| \\______/ \\__|  \\__| \\______/ \\__|       \\______/ \\________|\\__|          \n\n");
+            int nbPlayer;
+            string p;
+            do
             {
-                throw new Exception("Not enough player in the game");
+                Console.Write("Select the number of players: ");
+                p = Console.ReadLine();
+            } while (!int.TryParse(p, out nbPlayer) || nbPlayer <= 1);
+
+            for (int i = 1; i <= nbPlayer; i++)
+            {
+                Console.Write($"\nEntrer the name of Player N°{i}: ");
+                AddPlayer(new Player(Console.ReadLine()));
             }
 
             if (currentPlayer == null)
@@ -79,21 +85,31 @@ namespace MONOPOLY_LEDUC_GABISON.models
                 Random rnd = new Random();
                 indexCurrentPlayer = rnd.Next(0, players.Count);
                 currentPlayer = players[indexCurrentPlayer];
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"\n\n{currentPlayer.Name} starts the game!\n\n");
+                Console.ForegroundColor = ConsoleColor.White;
             }
+            
 
-            RollDice();
-            bool isTurnOver = currentPlayer.Move(dice[0] + dice[1], dice[0] == dice[1]);
-            Console.WriteLine(currentPlayer);
-            if (isTurnOver)
+            do
             {
-                // Replace with Iterator pattern
-                indexCurrentPlayer++;
-                if (indexCurrentPlayer >= players.Count)
+                RollDice();
+                bool isTurnOver = currentPlayer.Move(dice[0] + dice[1], dice[0] == dice[1]);
+                Console.WriteLine(currentPlayer);
+                if (isTurnOver)
                 {
-                    indexCurrentPlayer = 0;
+                    // Replace with Iterator pattern
+                    indexCurrentPlayer++;
+                    if (indexCurrentPlayer >= players.Count)
+                    {
+                        indexCurrentPlayer = 0;
+                    }
+                    currentPlayer = players[indexCurrentPlayer];
                 }
-                currentPlayer = players[indexCurrentPlayer];
-            }
+            } while (!IsGameOver());
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Congrats {GetWinner().Name} ! You're the winner !");
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public bool IsGameOver()
